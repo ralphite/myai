b59d11fa29fb26a0c4fee2a0af3c8e1b091b32a9
Ralphite <ralphite@users.noreply.github.com>
2024-04-09 11:47:07 -0700

    pypi config

--- reprod/gpt2_jax/utils.py
+++ reprod/gpt2_jax/utils.py
@@ -2,11 +2,10 @@ from dataclasses import dataclass
 from functools import cache
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-
 @cache
-def load_tokenizer():
+def load_hf_tokenizer():
     tokenizer_hf = GPT2Tokenizer.from_pretrained(
-        "openai-community/gpt2", cache_dir="./cache"
+        "openai-community/gpt2", cache_dir="./.hf_cache"
     )
 
     return tokenizer_hf
@@ -15,12 +14,12 @@ def load_tokenizer():
 @cache
 def load_hf_model():
     model_hf = GPT2LMHeadModel.from_pretrained(
-        "openai-community/gpt2", cache_dir="./cache"
+        "openai-community/gpt2", cache_dir="./.hf_cache"
     )
     return model_hf
 
 
 @cache
-def load_model_params():
+def load_pretrained_params():
     model_hf = load_hf_model()
     return {k: v.numpy() for k, v in model_hf.state_dict().items()}
--- reprod/gpt2_numpy/utils.py
+++ reprod/gpt2_numpy/utils.py
@@ -2,10 +2,11 @@ from dataclasses import dataclass
 from functools import cache
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
+
 @cache
-def load_hf_tokenizer():
+def load_tokenizer():
     tokenizer_hf = GPT2Tokenizer.from_pretrained(
-        "openai-community/gpt2", cache_dir="./cache"
+        "openai-community/gpt2", cache_dir="./.hf_cache"
     )
 
     return tokenizer_hf
@@ -14,12 +15,12 @@ def load_hf_tokenizer():
 @cache
 def load_hf_model():
     model_hf = GPT2LMHeadModel.from_pretrained(
-        "openai-community/gpt2", cache_dir="./cache"
+        "openai-community/gpt2", cache_dir="./.hf_cache"
     )
     return model_hf
 
 
 @cache
-def load_pretrained_params():
+def load_model_params():
     model_hf = load_hf_model()
     return {k: v.numpy() for k, v in model_hf.state_dict().items()}
